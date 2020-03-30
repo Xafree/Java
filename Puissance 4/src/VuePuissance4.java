78d5ce20dda04285db1742f044faadcf20cc5de8
Xafree <emmanueleberst@hotmail.com>
2020-03-30 02:43:40 +0200

    Fin vuePuissance4,Ajout de commentaire

--- Puissance 4/src/VuePuissance4.java
+++ Puissance 4/src/VuePuissance4.java
@@ -6,44 +6,54 @@ import javax.swing.JPanel;
 
 public class VuePuissance4 extends JPanel{
 	
-	 private JLabel joueurCourant;
-	 private JLabel nbTour;
-	 private JButton[][]plateau;
+	 public static  int LIGNE = 7; 		// Ligne MAX du plateau
+	 public static  int COLONNE = 6; 	// Colonne
+	 private JLabel joueurCourant; 		//JLabel qui prend le tour du joueur
+	 private JLabel nbTour;				// Variable qui récupère le nombre de tour
+	 private JButton[][]plateau; 		// Plateau du jeu
 
 	public VuePuissance4(){
-		// création du panel principal
 		this.setLayout(new BorderLayout());
+		
 		//création de l'entête de la fenêtre
 		JPanel nord = new JPanel(new GridLayout(1,3));
-		JButton recommencer = new JButton("Recommencer"); // bouton de reset
-		joueurCourant = new JLabel("Joueur 1"); // JLabel
+		
+		// Création des éléments
+		JButton recommencer = new JButton("Recommencer"); 
+		joueurCourant = new JLabel("Joueur 1"); 
 		nbTour = new JLabel("Tour 1");
+		
+		// Ajout des éléments dans le dans les Différent Panel
 		nord.add(recommencer);
 		nord.add(nbTour);
 		nord.add(joueurCourant);
 		this.add(nord,BorderLayout.NORTH);
 		
-		
+		// création d'un élement centrale 
 		JPanel centre = new JPanel(new GridLayout(7,6));
-		this.plateau = new JButton[7][6];
-		for(int i= 0; i < 7 ; i++){
-			for(int j= 0; j < 6; j++){
+		// création du plateau de jeu 
+		this.plateau = new JButton[LIGNE][COLONNE];
+		for(int i= 0; i < LIGNE ; i++){
+			for(int j= 0; j < COLONNE; j++){
 				this.plateau[i][j] = new JButton("");
 				this.plateau[i][j].setEnabled(false);
 				//this.plateau[i][j].addActionListener(controler);
+				//On l'ajoute dans le JPanel central
 				centre.add(this.plateau[i][j]);
 			}
 		}
-		
+		// Création des bouton de selection
 		for(int i  = 0 ; i < 7 ; i++) {
 			int j = i+1;
 			centre.add(new JButton(""+j+""));
 		}
-		
+		// Ajout des boutons de selection dans le JPanel
 		this.add(centre,BorderLayout.CENTER);
-
+		// création du panel sud (bas de fenetre)
 		JPanel sud = new JPanel(new GridLayout(1,6));
+		//JLabel de création de gagnant
 		JLabel etatPartie = new JLabel("Partie en cour !");
+		// Ajout des différents élément dans les Différents panel 
 		sud.add(etatPartie);
 		this.add(sud,BorderLayout.SOUTH);	
 		
