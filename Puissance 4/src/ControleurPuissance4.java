342d539b1dd755886e619f661fc6d82bcec59b4a
Xafree <emmanueleberst@hotmail.com>
2020-04-10 12:19:43 +0200

    Projet Finis

--- Puissance 4/src/ControleurPuissance4.java
+++ Puissance 4/src/ControleurPuissance4.java
@@ -16,14 +16,19 @@ public class ControleurPuissance4 implements ActionListener {
 	private ModelPuissance4 modele;
 	private	int score_joueur1 = 0;
 	private int score_joueur2 = 0;
-	private boolean retirerJeton;
+	private boolean retirerJetonJoueur1 ;
+	private boolean retirerJetonJoueur2 ;
+	int NbRetirerJetonUtiliserJoueur1 = 0;
+	int NbRetirerJetonUtiliserJoueur2 = 0;
 	
 	public ControleurPuissance4 (VuePuissance4 vue) {
 		this.vue = vue;
 		this.etatPartie = EtatPuissance4.JOUEUR1;
 		this.modele = new ModelPuissance4() ;
-		this.retirerJeton = false;
-		
+		this.retirerJetonJoueur1 = false;
+		this.retirerJetonJoueur2 = false;
+		this.NbRetirerJetonUtiliserJoueur1 = 0;
+		this.NbRetirerJetonUtiliserJoueur2 = 0;
 	}
 	
 	@Override
@@ -33,8 +38,7 @@ public class ControleurPuissance4 implements ActionListener {
 		Icon JetonJaune = new ImageIcon(caseJetonJaune);
 		URL caseJetonRouge = getClass().getResource("/Images/CaseRouge.png");
 		Icon JetonRouge = new ImageIcon(caseJetonRouge);
-		int NbRetirerJetonUtiliserJoueur1 = 0;
-		int NbRetirerJetonUtiliserJoueur2 = 0;
+
 		
 		JButton source  = (JButton)arg0.getSource();
 		if(!source.getText().equals("Recommencer")){
@@ -42,33 +46,30 @@ public class ControleurPuissance4 implements ActionListener {
 			switch(etatPartie) {
 			
 				case JOUEUR1:
-				
-					if(retirerJeton) {
+					
+					if(retirerJetonJoueur1 == true && NbRetirerJetonUtiliserJoueur1 < 1) {
 						vue.deplacerJetons(sourcePlacement.getIndexI());
 						modele.deplacerJeton(sourcePlacement.getIndexI());
 						etatPartie = EtatPuissance4.JOUEUR2;
-						this.retirerJeton = false;
+						NbRetirerJetonUtiliserJoueur1++;
+						vue.lockUnBouton(7);
+						this.retirerJetonJoueur1 = false;
 						
 					}else {
 						etatPartie = EtatPuissance4.JOUEUR2;
 						vue.setAvancerPartie(" Partie en cour ! ");
 						//Phase de test 
-						System.out.println("Colonne : "+sourcePlacement.getIndexI());
-						System.out.println("ligne   : "+modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexJ()));
-						System.out.println("Nombre jeton dans la colonne : "+(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI())-1)+"\n");
 						
+//						System.out.println("Colonne : "+sourcePlacement.getIndexI());
+//						System.out.println("ligne   : "+modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexJ()));
+//						System.out.println("Nombre jeton dans la colonne : "+(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI())-1)+"\n");
 						//Placement de pion 
-						if( NbRetirerJetonUtiliserJoueur1 > 0) {
-							vue.lockUnBouton(7);
-						}else {
-							vue.unLockUnBouton(7);
-						}
-						System.out.println(NbRetirerJetonUtiliserJoueur1);
+
+
 						if(sourcePlacement.getIndexI() == 7) {
-							this.retirerJeton = true;
+							this.retirerJetonJoueur1 = true;
 							etatPartie = EtatPuissance4.JOUEUR1;
 							sourcePlacement.setEnabled(false);
-							NbRetirerJetonUtiliserJoueur1++;
 							vue.setJoueurCourant("Joueur 1 à vous de jouer !");
 							vue.setAvancerPartie(" Joueur 1 retiré un jeton ");
 						}else if(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI()) == 0) {
@@ -79,36 +80,46 @@ public class ControleurPuissance4 implements ActionListener {
 							vue.setIconPlateau(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI()),sourcePlacement.getIndexI(),JetonJaune);	
 							vue.setJoueurCourant("Joueur 2 à vous de jouer !");
 						}
+						
 					}
 					
+					if( NbRetirerJetonUtiliserJoueur2 == 1) {
+						vue.lockUnBouton(7);
+					}else {
+						vue.unLockUnBouton(7);
+					}
 					System.out.println(modele.toString());
 					vue.setJoueurCourant("Joueur 2 à vous de jouer !");
 					
 					break;
 					
 				case JOUEUR2:
-					
-					System.out.println(NbRetirerJetonUtiliserJoueur2);
-					if(retirerJeton) {
+
+					if(retirerJetonJoueur2 && NbRetirerJetonUtiliserJoueur2 < 1) {
 						vue.deplacerJetons(sourcePlacement.getIndexI());
 						modele.deplacerJeton(sourcePlacement.getIndexI());
 						etatPartie = EtatPuissance4.JOUEUR1;
 						NbRetirerJetonUtiliserJoueur2++;
-						this.retirerJeton = false;
+						vue.lockUnBouton(7);
+						this.retirerJetonJoueur2 = false;
+						System.out.println("test5");
 					}else{
 						etatPartie = EtatPuissance4.JOUEUR1;
 						
 						//Phase de test 
-						System.out.println("Colonne : "+sourcePlacement.getIndexI());
-						System.out.println("ligne   : "+modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexJ()));
-						System.out.println("Nombre jeton dans la colonne : "+(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI())-1)+"\n");
+						
+//						System.out.println("Colonne : "+sourcePlacement.getIndexI());
+//						System.out.println("ligne   : "+modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexJ()));
+//						System.out.println("Nombre jeton dans la colonne : "+(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI())-1)+"\n");
 						
 						//Placement de pion 
 						if(sourcePlacement.getIndexI() == 7) {
-							this.retirerJeton = true;
+							this.retirerJetonJoueur2 = true;
+							System.out.println("test3");
 							etatPartie = EtatPuissance4.JOUEUR2;
-							//vue.lockUnBouton(7);
-							vue.setAvancerPartie("Retiré le dernier jeton d'un colonne");
+							System.out.println("test4");
+							vue.setAvancerPartie(" Joueur 2 retiré un jeton ");
+							vue.setJoueurCourant("Joueur 2 à vous de jouer !");
 						}else if(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI()) == 0) {
 							int colonne = sourcePlacement.getIndexI()+1;
 							vue.setAvancerPartie("Vous pouvez plus placer de jeton dans la colonne "+colonne);
@@ -117,6 +128,12 @@ public class ControleurPuissance4 implements ActionListener {
 							vue.setIconPlateau(modele.nbJetonPlacerDansColonnes(sourcePlacement.getIndexI()),sourcePlacement.getIndexI(),JetonRouge);	
 						}
 					}
+					
+					if( NbRetirerJetonUtiliserJoueur1 == 1) {
+						vue.lockUnBouton(7);
+					}else {
+						vue.unLockUnBouton(7);
+					}
 					vue.setJoueurCourant("Joueur 1 à vous de jouer !");
 					System.out.println(modele.toString());
 					break;			
@@ -124,7 +141,10 @@ public class ControleurPuissance4 implements ActionListener {
 		}else {
 			//Quand on appuie sur le bonton recommencer
 			modele.viderPlateau();
-			this.retirerJeton = false;
+			this.retirerJetonJoueur2 = false;
+			this.retirerJetonJoueur1= false;
+			this.NbRetirerJetonUtiliserJoueur2 = 0;
+			this.NbRetirerJetonUtiliserJoueur1 = 0;
 			vue.setAvancerPartie("Partie recommencer");
 			vue.unlockButton();
 			vue.resetPlateau();
@@ -136,18 +156,21 @@ public class ControleurPuissance4 implements ActionListener {
 		if(modele.PartieGagnerJaune()) {
 			this.score_joueur1 = this.score_joueur1+1;
 			vue.lockButton();
+			vue.lockUnBouton(7);
 			vue.setAvancerPartie("Joueur 1 vous avez gagner la partie !");
 			vue.setScore("Joueur 1 : "+score_joueur1+" | Joueur 2 : "+score_joueur2+"");
 			etatPartie = EtatPuissance4.JOUEUR1;
 		}else if(modele.PartieGagnerRouge()) {
 			this.score_joueur2 = this.score_joueur2+1;
 			vue.lockButton();
+			vue.lockUnBouton(7);
 			vue.setAvancerPartie("Joueur 2 vous avez gagner la partie !");
 			vue.setScore("Joueur 1 : "+score_joueur1+" | Joueur 2 : "+score_joueur2+"");
 			etatPartie = EtatPuissance4.JOUEUR2;
 		}else if(modele.egalite()){
 			vue.setAvancerPartie("Nous nous trouvons sur une égalité !");
 			vue.lockButton();
+			vue.lockUnBouton(7);
 		}
 	}
 }
